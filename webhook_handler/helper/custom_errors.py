790c5c5e29361fc9e17304a6c94722711318cb46
Liam Kontopulos <liam.kontopulos@gmail.com>
2025-09-05 11:12:36 +0200

    create custom errors

---
+++ webhook_handler/helper/custom_errors.py
@@ -0,0 +1,14 @@
+class DataMissingError(Exception):
+    """Raised whenever required data is missing"""
+
+    def __init__(
+        self, field: str, value: str, message: str = "Required data is missing"
+    ) -> None:
+        super().__init__(f"{message}: {field} = {value}")
+
+
+class ExecutionError(Exception):
+    """Raised whenever an error occurs during execution"""
+
+    def __init__(self, message: str = "An error occurred during execution") -> None:
+        super().__init__(message)
