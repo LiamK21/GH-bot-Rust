deb201adef2c29c18baf9b49497a4978961bed2e
Liam Kontopulos <liam.kontopulos@gmail.com>
2025-09-13 00:00:21 +0200

    refactor renamig script to pass repository for which scraped mocks should be added to test_data

--- rename_files.py
+++ rename_files.py
@@ -1,25 +1,27 @@
+import sys
 from pathlib import Path
 from typing import Literal
 
-type Repo = Literal["grcov", "rust-code-analysis"]
+type Repo = Literal["grcov", "rust-code-analysis", "glean", "neqo"]
 
-def move_and_rename_files():
+grcov_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "grcov")
+rust_code_analysis_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "rust-code-analysis")
+glean_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "glean")
+neqo_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "neqo")
+
+def move_and_rename_files(repos: list[Repo]):
     source_folder = Path(Path.cwd(), "scrape_mocks", "code_only").glob("*.json")
-    grcov_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "grcov")
-    rust_code_analysis_target_folder = Path(Path.cwd(), "webhook_handler", "test", "test_data", "rust-code-analysis")
-
     for file in source_folder:
-        file_name = file.as_posix().split("/")[-1]
-        if "grcov" in file_name:
-            identifier: Repo = "grcov"
-            target_folder = grcov_target_folder
-        elif "rust-code-analysis" in file_name:
-            identifier: Repo = "rust-code-analysis"
-            target_folder = rust_code_analysis_target_folder
+        file_name = file.stem
+        repository = file_name.split("_")[0]
+        if repository in repos:
+            target_folder = next((v for k, v in globals().copy().items() if k.startswith(f"{repository}")), None)
+            if not target_folder: raise Exception("No target folder found")
         else: 
-            raise Exception("No identifier found")
+            print(f"[!] Skipping file {file_name} as its repository {repository} is not in the provided list.")
+            continue
         
-        pr_num = file_name.split(".json")[0].lstrip(f"{identifier}_")
+        pr_num = file_name.split(".json")[0].lstrip(f"{repository}_")
         pr_num_int = int(pr_num)
         if not pr_num_int:
             raise Exception("No pr number found")
@@ -39,8 +41,35 @@ def move_and_rename_files():
         with open(absolute_file_path, mode="w") as f:
             f.write(file_content)
 
+def _validate_passed_args(args: list[str]) -> bool:
+    if len(args) == 0:
+        print("Usage: python rename_files.py [repository, repository...]")
+        print("Available repositories: grcov, rust-code-analysis, glean, neqo")
+        sys.exit(1)
+   
+    valid_repos = ["grcov", "rust-code-analysis", "glean", "neqo"]
+    for arg in args:
+        if arg not in valid_repos:
+            print(f"[!] Invalid repository: {arg}")
+            print("Usage: python python rename_files.py [repository, repository...]")
+            print("Available repositories: grcov, rust-code-analysis, glean, neqo")
+            sys.exit(1)
+    return True
+  
             
 if __name__ == "__main__":
+    repos: list[Repo] = []
+    cli_args = sys.argv[1:]
+    _validate_passed_args(cli_args)
+    
+    if "grcov" in cli_args:
+        repos.append("grcov")
+    if "rust-code-analysis" in cli_args:
+        repos.append("rust-code-analysis")
+    if "glean" in cli_args:
+        repos.append("glean")
+    if "neqo" in cli_args:
+        repos.append("neqo")
     print("[*] Moving and renaming files...")
-    move_and_rename_files()
+    move_and_rename_files(repos)
     print("[*] Moving and renaming successful.")
