5a936e23eac50f76ab27fb6c3a0702e3939da938
Liam Kontopulos <liam.kontopulos@gmail.com>
2025-09-13 23:56:37 +0200

    add support for bugzilla issues

--- scrape_pr.py
+++ scrape_pr.py
@@ -5,7 +5,7 @@ import sys
 import time
 from enum import StrEnum
 from pathlib import Path
-from typing import Literal
+from typing import Literal, cast
 
 import requests
 from dotenv import load_dotenv
@@ -34,6 +34,13 @@ GITHUB_HEADERS = {
     "Accept": "application/json",
     "Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}",
 }
+BUGZILLA_GET_BUG_URL = "https://bugzilla.mozilla.org/rest/bug"
+BUGZILLA_BUGS: list[int] = []
+BUGZILLA_BUGS_DICT = dict() 
+BUGZILLA_BUGS_REQUIRED = 50
+BUGZILLA_HEADERS = {
+    "Accept": "application/json",
+}
 
 
 def _fetch_github_data(url: str) -> dict:
@@ -57,7 +64,7 @@ def _fetch_github_data(url: str) -> dict:
     response.raise_for_status()
     return response.json()
 
-def _fetch_bugzilla_data(bug_id: int) -> dict:
+def _fetch_bugzilla_data() -> dict:
     """
     Fetches data from Bugzilla API.
 
@@ -67,17 +74,20 @@ def _fetch_bugzilla_data(bug_id: int) -> dict:
     Returns:
         dict: Data.
     """
-    url = f"https://bugzilla.mozilla.org/rest/bug/{bug_id}"
-    response = requests.get(url)
+    bugs_str = ",".join(str(bug) for bug in BUGZILLA_BUGS)
+    print("[*] Sleeping for 60 seconds before Bugzilla API call...")
+    time.sleep(60)  # to avoid hitting rate limits
+    url = f"https://bugzilla.mozilla.org/rest/bug?id={bugs_str}&include_fields=id,summary,component,description"
+    response = requests.get(url, headers=BUGZILLA_HEADERS)
     if response.status_code == 403 and "X-RateLimit-Reset" in response.headers:
         print("[*] Sleeping...")
         reset_time = int(response.headers["X-RateLimit-Reset"])
         wait_time = reset_time - int(time.time()) + 1
         time.sleep(max(wait_time, 1))
-        return _fetch_bugzilla_data(bug_id)
+        return _fetch_bugzilla_data()
     response.raise_for_status()
+    
     return response.json()
-
 
 def _fetch_pr_list(curr_page: int, repo: Repo) -> dict:
     """
@@ -112,7 +122,38 @@ def _fetch_pr_files(pr_number: int, repo: Repo) -> dict:
     return _fetch_github_data(url)
 
 
-def _get_linked_data(pr_title: str, pr_description: str, repo: Repo) -> str:
+def _get_linked_bugzilla_data(pr_description: str, is_pre_processing: bool) -> str:
+    bugzilla_url_pattern = r"\bhttps://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)\b"
+    bugzilla_bug_id_pattern = r"\bbug\s+(\d+)\b"
+    bugzilla_url_matches: list[str] = re.findall(
+        bugzilla_url_pattern, pr_description, re.IGNORECASE
+    )
+    bugzilla__bug_id_matches: list[str] = re.findall(
+        bugzilla_bug_id_pattern, pr_description, re.IGNORECASE
+    )
+    all_matches = bugzilla_url_matches + bugzilla__bug_id_matches
+    
+    if all_matches:
+        for bug_id in all_matches:
+            bug_id = int(bug_id)
+            if not bug_id:
+                continue
+            
+            if is_pre_processing:
+                BUGZILLA_BUGS.append(bug_id)
+                if len(BUGZILLA_BUGS) >= BUGZILLA_BUGS_REQUIRED:
+                    _add_bugzilla_issues()
+                return "BUGZILLA"
+            else:
+                # retrieve from already fetched bugs
+                linked_issue_description = BUGZILLA_BUGS_DICT.get(bug_id)
+                del(BUGZILLA_BUGS_DICT[bug_id])
+                if linked_issue_description:
+                    return linked_issue_description
+    return ""
+
+
+def _get_linked_data(pr_title: str, pr_description: str, repo: Repo, is_pre_processing: bool = False) -> str:
     """
     Checks and fetches a linked issue.
 
@@ -126,24 +167,9 @@ def _get_linked_data(pr_title: str, pr_description: str, repo: Repo) -> str:
     issue_description = f"{pr_title} {pr_description}"
     # Glean often links to bugs in Bugzilla, not GitHub issues
     if repo == "glean":
-        bugzilla_url_pattern = r"\bhttps://bugzilla\.mozilla\.org/show_bug\.cgi\?id=(\d+)\b"
-        bugzilla_bug_id_pattern = r"\bbug\s+(\d+)\b"
-        bugzilla_url_matches: list[str] = re.findall(
-            bugzilla_url_pattern, issue_description, re.IGNORECASE
-        )
-        bugzilla__bug_id_matches: list[str] = re.findall(
-            bugzilla_bug_id_pattern, issue_description, re.IGNORECASE
-        )
-        all_matches = bugzilla_url_matches + bugzilla__bug_id_matches
-        
-        if all_matches:
-            for bug_id in all_matches:
-                bug_id = int(bug_id)
-                if not bug_id:
-                    continue
-                linked_issue_description = _get_bugzilla_issue(bug_id)
-                if linked_issue_description:
-                    return linked_issue_description
+        val = _get_linked_bugzilla_data(issue_description, is_pre_processing)
+        if val:
+            return val
                 
     issue_pattern = r"#(\d+)"
     url_pattern = rf"\bhttps://github\.com/mozilla/{re.escape(repo)}/issues/(\d+)\b"
@@ -172,26 +198,30 @@ def _get_linked_data(pr_title: str, pr_description: str, repo: Repo) -> str:
     return ""
 
 
-def _get_bugzilla_issue(number: int) -> str | None:
+def _add_bugzilla_issues() -> None:
     """
-    Fetches a Bugzilla issue.
-
-    Parameters:
-        number (int): The number of the issue
-
-    Returns:
-        str | None: The Bugzilla issue title and description
+    Fetches Bugzilla issues and stores them in the BUGZILLA_BUG_DICT.
     """
-    bug_data = _fetch_bugzilla_data(number)
+    bug_data = _fetch_bugzilla_data()
     if "bugs" in bug_data and len(bug_data["bugs"]) > 0:
-        bug: dict = bug_data["bugs"][0]
-        component: str = bug.get("component", "")
-        if not "glean" in component.lower():
-            return None
-        return "\n".join(
-            value for value in (bug["summary"], bug.get("description", "")) if value
-        )
-    return None
+        for bug in bug_data["bugs"]:
+            bug_id: int = bug.get("id")
+            if not bug_id:
+                print(f"[!] Invalid Bugzilla bug ID: {bug_id}")
+                continue
+            
+            BUGZILLA_BUGS.remove(bug_id)
+            component: str = bug.get("component", "")
+            # We are only interested in Glean bugs
+            if not "glean" in component.lower():
+                continue
+            # Bug ID should be numeric
+            summary, descrption =  bug.get("summary", ""), bug.get("description", "")
+            # Both summary and description should not be empty
+            if not summary and not descrption:
+                continue
+            BUGZILLA_BUGS_DICT[int(bug_id)] =  summary + "\n" + descrption
+
 
 def _get_github_issue(number: int, repo: Repo) -> str | None:
     """
@@ -304,25 +334,37 @@ def _save_pr_amp(payload, repo: Repo) -> None:
         print(f"[!] PR #{pr_number} at {path} already exists\n")
 
 
-def _process_pr(curr_pr: dict, repo: Repo) -> bool | str:
+def pre_process_glean_prs(pr_list: dict):
+    copied_pr_list = pr_list.copy()
+    for pr in copied_pr_list:
+        pr_number, is_valid_pr = _validate_pr(pr)
+        if not is_valid_pr:
+            cast(list, pr_list).remove(pr)
+            continue
+        res = _get_linked_data(pr["title"], pr["body"], "glean", True)
+        
+        if not res:
+            print(f"[!] Neither GitHub nor Bugzilla linked issue for PR #{pr_number}\n")
+            cast(list, pr_list).remove(pr)
+            continue
+        
+        if res == "BUGZILLA":
+            print(f"[*] linked issue belongs to Bugzilla")
+            cast(list, pr_list).remove(pr)
+    
+def process_pr(curr_pr: dict, repo: Repo) -> bool | str:
     """
     Processes PR fetched from the GitHub PR list.
-
+    The repository cannot be glean.
+    
     Parameters:
         curr_pr (dict): PR data.
 
     Returns:
         bool: True if PR fulfills requirements, False otherwise.
     """
-    pr_number = curr_pr["number"]
-    print(f"[*] Processing PR #{pr_number}...")
-
-    # Requirement #1: PR must have action OPENED or MERGED (=CLOSED and MERGED_AT)
-    if not (
-        curr_pr["state"] == "open"
-        or (curr_pr["state"] == "closed" and curr_pr["merged_at"] is not None)
-    ):
-        print(f"[!] PR #{pr_number} was closed but not merged\n")
+    pr_number, is_valid_pr = _validate_pr(curr_pr)
+    if not is_valid_pr:
         return False
 
     # Requirement #2: PR must have linked issue
@@ -331,9 +373,9 @@ def _process_pr(curr_pr: dict, repo: Repo) -> bool | str:
         return False
 
     files = _fetch_pr_files(pr_number, repo)
-    file_types = []
+    file_types: list[FileType] = []
     for f in files:
-        filename = f["filename"]
+        filename: str = f["filename"]
         if "patch" in f:
             if _is_test_file(filename):
                 file_types.append(FileType.TEST)
@@ -348,7 +390,7 @@ def _process_pr(curr_pr: dict, repo: Repo) -> bool | str:
 
     # Perhaps we can relax this requirement to at least one src file
     # Requirement #3: All .rs files modified in PR must be source code files
-    if "non-src" in file_types:
+    if FileType.NON_SRC in file_types:
         print(f"[!] Non-source code files in PR #{pr_number}\n")
         return False
 
@@ -360,8 +402,8 @@ def _process_pr(curr_pr: dict, repo: Repo) -> bool | str:
     }
 
     # Requirement #4: PR must modify at least one .rs file
-    if "src" in file_types:
-        if "test" in file_types:
+    if FileType.SRC in file_types:
+        if FileType.TEST in file_types:
             _save_pr_amp(current_payload, repo)
             return "amp"
         else:
@@ -370,6 +412,25 @@ def _process_pr(curr_pr: dict, repo: Repo) -> bool | str:
     else:
         print(f"[!] No .rs changes in source code in PR #{pr_number}\n")
         return False
+
+
+def _validate_pr(curr_pr: dict) -> tuple[int, bool]:
+    pr_number = curr_pr["number"]
+    if not isinstance(pr_number, int):
+        return 0, False
+    print(f"[*] Processing PR #{pr_number}...")
+    
+    is_valid: bool = True
+
+    # Requirement #1: PR must have action OPENED or MERGED (=CLOSED and MERGED_AT)
+    if not (
+        curr_pr["state"] == "open"
+        or (curr_pr["state"] == "closed" and curr_pr["merged_at"] is not None)
+    ):
+        print(f"[!] PR #{pr_number} was closed but not merged\n")
+        is_valid = False
+        
+    return pr_number, is_valid
 
 def _validate_passed_args(args: list[str]) -> bool:
     if len(args) == 0:
@@ -420,9 +481,19 @@ if __name__ == "__main__":
             if not pr_list:
                 print("[*] No PRs to process")
                 break
+            if repo == "glean":
+                print("[*] Pre-processing the PR list for repository glean")
+                pre_process_glean_prs(pr_list)
+            
             
             for pr in pr_list:
-                res = _process_pr(pr, repo)
+                if repo == "glean":
+                    res: str | bool = False
+                    # Call different starting function for Glean to batch fetch Bugzilla bugs
+                    pass
+                else:
+                    res = process_pr(pr, repo)
+                    
                 if res and (valid_payloads) >= SCRAPE_TARGET:
                     break
                 curr_valid_payloads += 1 if res == "src" else 0
