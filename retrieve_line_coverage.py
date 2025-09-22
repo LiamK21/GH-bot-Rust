d94bd1ddc0dd0297055ff3f9f6fd86e8376c0420
Liam Kontopulos <liam.kontopulos@gmail.com>
2025-09-22 13:53:21 +0200

    add script for running inside docker container to retrieve line coverage

---
+++ retrieve_line_coverage.py
@@ -0,0 +1,41 @@
+import json
+import os
+import sys
+import time
+from pathlib import Path
+
+
+def main(rel_filename: str) -> float:
+    path_to_json = Path(Path.cwd(), "coverage.json")
+    timeout = 10
+    while not path_to_json.exists() and timeout > 0:
+        timeout -= 1
+        time.sleep(1)
+    if not path_to_json.exists():
+        raise Exception("Coverage file not found")    
+    with open(path_to_json, "r") as f:
+        data = json.load(f)
+
+    # First "data" entry contains coverage results
+    coverage_data = data["data"][0]
+
+    for file in coverage_data["files"]:
+        fname = file["filename"]
+        if rel_filename not in fname:
+            continue
+        
+        lines = file["summary"]["lines"]
+        percent: float = lines["percent"]
+        return percent
+    
+    raise Exception(f"File {rel_filename} not found in coverage data")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python fun.py <filename>")
+        sys.exit(1)
+        
+    filename = sys.argv[1]
+    val = main(filename)
+    print(f"{val:.2f}")
