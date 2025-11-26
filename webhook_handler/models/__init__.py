ef17c9d06d6a82daf43eb32389b6e6a4551df5a3
Liam Kontopulos <liam.kontopulos@gmail.com>
2025-11-26 08:08:28 +0100

    make test coverage importable

--- webhook_handler/models/__init__.py
+++ webhook_handler/models/__init__.py
@@ -4,5 +4,6 @@ from .pipeline_inputs import PipelineInputs
 from .pr_data import PullRequestData
 from .pr_file_diff import PullRequestFileDiff
 from .prompt_type_enum import PromptType
+from .test_coverage import TestCoverage
 
-__all__ = ["LLM", "PullRequestData", "PullRequestFileDiff", "PipelineInputs", "PromptType", "LLMResponse"]
+__all__ = ["LLM", "PullRequestData", "PullRequestFileDiff", "PipelineInputs", "PromptType", "LLMResponse", "TestCoverage"]
